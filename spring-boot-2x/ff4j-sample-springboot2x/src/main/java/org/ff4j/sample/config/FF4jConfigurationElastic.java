b3a229a17e8dabb685d95d6eaef00dfc056ffb46
clunven <cedrick.lunven@gmail.com>
2020-05-02 18:11:46 +0200

    simple

---
+++ spring-boot-2x/ff4j-sample-springboot2x/src/main/java/org/ff4j/sample/config/FF4jConfigurationElastic.java
@@ -0,0 +1,51 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2013-2016 the original author or authors.
+ */
+
+package org.ff4j.sample.config;
+
+import org.ff4j.FF4j;
+import org.ff4j.audit.repository.InMemoryEventRepository;
+import org.ff4j.property.store.InMemoryPropertyStore;
+import org.ff4j.store.InMemoryFeatureStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Definition of FF4j Bean. This definition should be done not only
+ * in the backend with web console and rest API but also in your microservices.
+ */
+@Configuration
+public class FF4jConfigurationElastic {
+    
+    /**
+     * Initiate ff4j pointing to Elastic.
+     *
+     * Default index is ff4j_features but you can override the indexName
+     * ff4j.setFeatureStore(new FeatureStoreElastic(jestClient, "indexName"));
+     * 
+     * @param jestClient
+     * @return
+     */
+    @Bean
+    public FF4j getFF4j() {
+        FF4j ff4j = new FF4j();
+        ff4j.setFeatureStore(new InMemoryFeatureStore());
+        ff4j.setPropertiesStore(new InMemoryPropertyStore());
+        ff4j.setEventRepository(new InMemoryEventRepository());
+        // we enable audit to have KPI in the monitoring 
+        ff4j.audit(true);
+        return ff4j;
+    }
+}
+
